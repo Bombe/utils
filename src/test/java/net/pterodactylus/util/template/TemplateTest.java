5456dc2ef39ffcd246cfefd3ecd8cb5dd0909875
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-02-20 02:05:31 +0100

    Add test case for <%between>.

--- src/test/java/net/pterodactylus/util/template/TemplateTest.java
+++ src/test/java/net/pterodactylus/util/template/TemplateTest.java
@@ -366,4 +366,33 @@ public class TemplateTest extends TestCase {
 		assertEquals("List: Yay! Hooray! ", output);
 	}
 
+	/**
+	 * Tests for string templates that contain a collection and the <%between>
+	 * directive.
+	 *
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 * @throws TemplateException
+	 */
+	public void testStringTemplatesWithCollectionBetween() throws IOException, TemplateException {
+		Template template;
+		String templateString;
+		StringWriter outputWriter;
+		String output;
+		Collection<Object> collection;
+
+		templateString = "List: <%foreach items item><%item><%between>, <%/between><%last>.<%/last><%/foreach>";
+		outputWriter = new StringWriter();
+		template = new Template(new StringReader(templateString));
+		collection = new ArrayList<Object>();
+		collection.add(1);
+		collection.add(2);
+		collection.add(3);
+		collection.add(4);
+		template.set("items", collection);
+		template.render(outputWriter);
+		output = outputWriter.toString();
+		assertEquals("List: 1, 2, 3, 4.", output);
+	}
+
 }
