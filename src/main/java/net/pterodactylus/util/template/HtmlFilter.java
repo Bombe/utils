77e66cd5762fc44b6653987bb024f828a1edc354
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-02-22 08:22:28 +0100

    Add missing file comments.

--- src/main/java/net/pterodactylus/util/template/HtmlFilter.java
+++ src/main/java/net/pterodactylus/util/template/HtmlFilter.java
@@ -1,12 +1,29 @@
+/*
+ * utils - HtmlFilter.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.pterodactylus.util.template;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Filters HTML by replacing all characters that match a defined HTML entity
- * by that entity. Unknown characters that are outside of the US-ASCII range
- * (0 to 127) are encoded using the {@code &amp;#1234;} syntax.
+ * Filters HTML by replacing all characters that match a defined HTML entity by
+ * that entity. Unknown characters that are outside of the US-ASCII range (0 to
+ * 127) are encoded using the {@code &amp;#1234;} syntax.
  *
  * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
  */
