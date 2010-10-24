dd77275ac4b94ded37cc7eeec9e1d6eaa270f888
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2010-10-24 17:59:01 +0200

    Add helper method collection for dealing with numbers.

---
+++ src/main/java/net/pterodactylus/util/number/Numbers.java
@@ -0,0 +1,64 @@
+/*
+ * utils - Numbers.java - Copyright © 2010 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.util.number;
+
+/**
+ * Collection of various helper methods that deal with numbers.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class Numbers {
+
+	/**
+	 * Tries to parse the {@link String} representation of the given object (as
+	 * per {@link String#valueOf(Object)}) as an {@link Integer}.
+	 *
+	 * @param object
+	 *            The object to parse
+	 * @return The parsed {@link Integer}, or {@code null} if the object could
+	 *         not be parsed
+	 */
+	public static Integer safeParseInteger(Object object) {
+		return safeParseInteger(object, null);
+	}
+
+	/**
+	 * Tries to parse the {@link String} representation of the given object (as
+	 * per {@link String#valueOf(Object)}) as an {@link Integer}.
+	 *
+	 * @param object
+	 *            The object to parse
+	 * @param defaultValue
+	 *            The value to return if the object is {@code null} or can not
+	 *            be parsed as an {@link Integer}
+	 * @return The parsed Integer, or {@code null} if the object could not be
+	 *         parsed
+	 */
+	public static Integer safeParseInteger(Object object, Integer defaultValue) {
+		if (object == null) {
+			return defaultValue;
+		}
+		try {
+			return Integer.parseInt(String.valueOf(object));
+		} catch (NumberFormatException nfe1) {
+			/* ignore. */
+		}
+		return defaultValue;
+	}
+
+}
