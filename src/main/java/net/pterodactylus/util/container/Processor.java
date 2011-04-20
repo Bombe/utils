3c6fbfffb49616379c16b62b97df4acb9017945a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-04-20 11:21:39 +0200

    Add processor interface.

---
+++ src/main/java/net/pterodactylus/util/container/Processor.java
@@ -0,0 +1,38 @@
+/*
+ * utils - Processor.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.util.container;
+
+/**
+ * A processor is used to process all elements of a {@link Container}.
+ *
+ * @see Container#process(Processor)
+ * @param <T>
+ *            The type of the elements to process
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public interface Processor<T> {
+
+	/**
+	 * Processes the given object.
+	 *
+	 * @param object
+	 *            The object to process
+	 */
+	public void process(T object);
+
+}
