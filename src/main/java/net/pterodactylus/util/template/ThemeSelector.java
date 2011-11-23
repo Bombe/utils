202519b192c2b538ca75e367a194f4be4644cb3f
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2011-11-23 08:42:59 +0100

    Add theme selector interface.

---
+++ src/main/java/net/pterodactylus/util/template/ThemeSelector.java
@@ -0,0 +1,40 @@
+/*
+ * utils - ThemeSelector.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.util.template;
+
+/**
+ * Selects themes based on a context.
+ *
+ * @param <C>
+ *            The type of the context
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public interface ThemeSelector<C extends ThemeSelectorContext> {
+
+	/**
+	 * Returns a theme for the given context. This method should always return a
+	 * theme! If the context does not contain information about a theme which
+	 * should be selected a default theme should be returned.
+	 *
+	 * @param context
+	 *            The context to get the theme selection parameters from
+	 * @return The selected theme
+	 */
+	public Theme getTheme(C context);
+
+}
---
+++ src/main/java/net/pterodactylus/util/template/ThemeSelectorContext.java
@@ -0,0 +1,33 @@
+/*
+ * utils - ThemeSelectorContext.java - Copyright © 2011 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.util.template;
+
+/**
+ * Marker interface for {@link ThemeSelector} contexts. A theme selector context
+ * can contain arbitrary data that is used to
+ * {@link ThemeSelector#getTheme(ThemeSelectorContext) select a theme}, such as
+ * an HTTP request object which can contain parameters or session information
+ * which allow the theme selector to choose an appropriate theme.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class ThemeSelectorContext {
+
+	/* no methods. */
+
+}
